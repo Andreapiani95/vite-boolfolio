8c7443645490ee49873940c03422227cf44b6426
Andreapiani95 <andreapiani1995@gmail.com>
2023-09-28 17:40:38 +0200

    milestone 3+4

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'MainComponent',
+    data() {
+        return {
+            projects: []
+        }
+    },
+    created() {
+        axios
+            .get('http://localhost:8000/api/projects')
+            .then(res => {
+                console.log(res);
+            });
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<template>
+    <h2>MainComponent</h2>
+</template>
+
+<style lang="scss"></style>
